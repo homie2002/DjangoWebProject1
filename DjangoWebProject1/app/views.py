1290680061925693366f759126a1a976161b16bf
Nikita <shko1ik228@gmail.com>
2023-09-03 11:15:47 +0300

    1

---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,33 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpRequest
+import speedtest
+
+
+def home(request):
+    download_speed = None
+    upload_speed = None
+    
+    if request.method == 'POST':
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        
+        # Запустите измерение скорости. Мы не возвращаем результаты здесь.
+        st.download()
+        st.upload()
+        
+        download_speed = st.results.download / 1024 / 1024  # Скорость загрузки в Mbps
+        upload_speed = st.results.upload / 1024 / 1024  # Скорость загрузки в Mbps
+    
+    return render(
+        request,
+        'app/index.html',
+        {
+            'download_speed': download_speed,
+            'upload_speed': upload_speed,
+        }
+    )
